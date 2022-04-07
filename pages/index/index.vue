97ffb2b8349d6edabbbd73f1e9510726e459958e
托噜酱 <58923223+tonylu110@users.noreply.github.com>
2022-04-07 12:31:25 +0800

    Update: pull down to load more videos

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -37,16 +37,40 @@
 			} else {
 				this.mobile = 'other'
 			}
-			console.log(uni.getSystemInfoSync().platform)
-			uni.request({
-				url:'https://bili.tnyl.xyz/index/ding.json',
-				success: (res) => {
-					console.log(res.data)
-					this.list_main = res.data.douga
-				}
-			})
+			this.getList()
+		},
+		onPullDownRefresh() {
+			setTimeout(() => {
+				uni.stopPullDownRefresh();
+				this.getList()
+			}, 1000);
+		},
+		onReachBottom () {
+			setTimeout(() => {
+				this.getList(true)
+			},1500)
 		},
 		methods: {
+			getList(bottom) {
+				if (!bottom) {
+					uni.request({
+						url:'https://bili.tnyl.xyz/index/ding.json',
+						success: (res) => {
+							this.list_main = res.data.douga
+						}
+					})
+				} else {
+					var list = []
+					list = Object.values(this.list_main)
+					uni.request({
+						url:'https://bili.tnyl.xyz/index/ding.json',
+						success: (res) => {
+							let more_list = Object.values(res.data.douga)
+							this.list_main = [...list, ...more_list]
+						}
+					})
+				}
+			},
 			search() {
 				uni.navigateTo({
 					url:'../search/search'
