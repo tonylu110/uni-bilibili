1da477186f000f48316f34e1a8e0d05a6e9449cd
托噜酱 <58923223+tonylu110@users.noreply.github.com>
2022-04-12 09:12:12 +0800

    Add: mine page

--- pages/mine/mine.vue
+++ pages/mine/mine.vue
@@ -1,6 +1,24 @@
 <template>
-	<view>
-		
+	<view class="content">
+		<image src="../../static/logo.png" mode="" class="user_img"></image>
+		<view class="user_info" v-if="userLogin">
+			<view class="">
+				动态
+			</view>
+			<view class="">
+				关注
+			</view>
+			<view class="">
+				粉丝
+			</view>
+		</view>
+		<view class="user_list" v-if="userLogin">
+			<navigator hover-stay-time="0" url="" class="list">我的空间</navigator>
+			<navigator hover-stay-time="0" url="" class="list">历史记录</navigator>
+			<navigator hover-stay-time="0" url="" class="list">我的收藏</navigator>
+			<navigator hover-stay-time="0" url="" class="list">设置</navigator>
+		</view>
+		<button type="primary">登录</button>
 	</view>
 </template>
 
@@ -8,7 +26,7 @@
 	export default {
 		data() {
 			return {
-				
+				userLogin: false
 			}
 		},
 		methods: {
@@ -18,5 +36,5 @@
 </script>
 
 <style>
-
+	@import url("./mine.css");
 </style>
